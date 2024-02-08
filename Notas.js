d9e655c6dc3af10b2f943d5361bc7e4059ac941f
leomartins10 <leonardo.martins@sou.inteli.edu.br>
2024-02-08 12:58:26 -0300

    Initial commit

---
+++ Notas.js
@@ -0,0 +1,19 @@
+
+var nota1 = 7;
+var nota2 = 8;
+var nota3 = 9;
+var nota4 = 10;
+var mediaDoColegio = 6;
+var mediaDoAluno = (nota1 + nota2 + nota3 + nota4)/4
+console.log("Notado Primeiro Bimestre:", nota1);
+console.log("Nota do Segundo Bimestre:", nota2);
+console.log("Nota do Terceiro Bimestre:", nota3);
+console.log("Nota do Quarto Bimestre:", nota4);
+console.log("Média de Todos os Bimestres:", mediaDoAluno);
+if (mediaDoAluno >= 7){
+    console.log ("Parabéns, você foi APROVADO!");
+} else {
+    console.log("Que pena, você está de recuperação...");
+}
+
+
